6d9974741612537fcdb2b7e17f8345ea67e4ab5c
joshkaminski <107281803+joshkaminski@users.noreply.github.com>
2022-10-10 20:33:07 -0400

    First commit

---
+++ expo-app/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        onPress={() => navigation.navigate("Other")}
+        title="Change Screens"
+        color="#841584"
+       />
+    </View>
+  );
+}
+
+function OtherScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Other Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Other" component={OtherScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
